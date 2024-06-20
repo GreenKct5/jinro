76319c428fd65ca62fb8196e0028da60c47d6ebe
kototo522 <huyejiben@gmail.com>
2024-06-20 09:26:59 +0900

    add: client_mとserver_mファイルの追加

---
+++ client_m.c
@@ -0,0 +1,95 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <sys/select.h>
+#include <time.h>
+
+#define PORT                (in_port_t)50000
+#define BUF_LEN             512
+
+char * chop_newline(char *str,int len){
+    int n = strlen(str);
+    if(n<len && str[n-1]=='\n'){
+        str[n-1] = '\0';
+    }
+    return str;
+}
+
+void main()
+{
+    struct sockaddr_in server;
+    int soc;
+    char ip_str[BUF_LEN];
+    struct in_addr ip_addr;
+    char buf[BUF_LEN];
+    char username[BUF_LEN],partnername[BUF_LEN];
+
+    /*
+    input server's ip 
+    */
+    write(1,"Please input server's address : ",strlen("Please input server's address : "));
+    read(0,ip_str,BUF_LEN);
+
+    write(1,"Please input your name        : ",strlen("Please input your name        : "));
+    read(0,username,BUF_LEN);
+    chop_newline(username,BUF_LEN);
+
+    inet_aton(ip_str,&ip_addr);                 // address(text) to network address(binary)
+    memset((char *)&server,0,sizeof(server));
+    server.sin_family = AF_INET;
+    server.sin_port = htons(PORT);
+    memcpy((char *)&server.sin_addr,&ip_addr,sizeof(ip_addr));
+
+    if((soc = socket(AF_INET,SOCK_STREAM,0)) < 0){
+        perror("socket");
+        exit(1);
+    }
+    
+    if(connect(soc,(struct sockaddr *)&server,sizeof(server)) == -1){
+        perror("connect");
+        exit(1);
+    }
+    write(1,"Go Ahead!\n",strlen("Go Ahead!"));
+    // await-async chat 
+    fd_set readset,readset_origin;
+    int fd = soc;
+    
+    FD_ZERO(&readset);
+
+    FD_SET(0,&readset);
+    FD_SET(fd,&readset);
+    readset_origin = readset;
+
+    do{
+    readset = readset_origin;
+    select(fd+1,&readset,NULL,NULL,NULL);
+        if(FD_ISSET(0,&readset)){
+            time_t now;
+            struct tm *t;
+            time(&now);
+            t = localtime(&now);
+            char time[256];
+            int timeLen = strftime(time,256,", %X)-> ",t);
+
+            int n = read(0,buf,BUF_LEN);
+
+            write(fd,"(",strlen("("));
+            write(fd,username,strlen(username));
+            write(fd,time,timeLen);
+            write(fd,buf,n);
+        }
+        if(FD_ISSET(fd,&readset)){
+            int n;
+            n = read(fd,buf,BUF_LEN);
+            write(1,buf,n);
+        }
+    }while(strncmp(buf,"quit",4) != 0);
+    
+    close(soc);
+}
---
+++ server_m.c
@@ -0,0 +1,76 @@
+#include <sys/select.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <time.h>
+
+#define PORT (in_port_t)    50000
+#define BUF_LEN             512
+
+char * chop_newline(char *str,int len){
+    int n = strlen(str);
+    if(n<len && str[n-1]=='\n'){
+        str[n-1] = '\0';
+    }
+    return str;
+}
+
+void main()
+{
+    struct sockaddr_in me;
+    int playerNum;
+    int soc_waiting;
+    char buf[BUF_LEN];
+    char username[BUF_LEN],partnername[BUF_LEN];
+    char message[] = "Please input your name        : "; 
+    write(1,message,strlen(message));
+    read(0,username,BUF_LEN);
+    chop_newline(username,BUF_LEN);
+    write(1,"Please input player num    :",strlen("Please input player num    :"));
+    scanf("%d",&playerNum);
+    
+    memset((char *)&me,0,sizeof(me));           // initialize "me"
+    me.sin_family = AF_INET;                    // configure protocol (AF_INET: IPv4)
+    me.sin_addr.s_addr = htonl(INADDR_ANY);     // configure own IP Address (INADDR_ANY: 0.0.0.0)
+    me.sin_port = htons(PORT);                  // configure port number
+
+    if((soc_waiting = socket(AF_INET,SOCK_STREAM,0)) < 0){
+        perror("socket");
+        exit(1);
+    }
+    
+    if(bind(soc_waiting,(struct sockaddr *)&me,sizeof(me)) == -1){
+        perror("bind");
+        exit(1);
+    }
+
+    listen(soc_waiting,playerNum);
+    int soc_comm[playerNum];
+    for ( int i = 0; i < playerNum; i++) soc_comm[i] = accept(soc_waiting,NULL,NULL);
+    
+    close(soc_waiting);
+
+    // await-async chat 
+    fd_set readset,readset_origin;
+    FD_ZERO(&readset);
+    for(int i = 0; i < playerNum; i++) FD_SET(soc_comm[i],&readset);
+    readset_origin = readset;
+    do{
+        readset = readset_origin;
+        select(soc_comm[playerNum-1]+1,&readset,NULL,NULL,NULL);
+        for (int i = 0; i < playerNum; i++){
+            if(FD_ISSET(soc_comm[i],&readset)){
+                int n = read(soc_comm[i],buf,BUF_LEN);
+                for (int j = 1;j < playerNum;j++) write(soc_comm[(i+j)%playerNum],buf,n);
+                write(1,buf,n);
+            }
+        }
+    }while(strncmp(buf,"quit",4) != 0);
+    
+    for(int i = 0;i < playerNum;i++) close(soc_comm[i]);
+}
