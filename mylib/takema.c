f4c4a60ff7047072ef4bd2850874025b0a0a1b68
TakayamaH <823ab172@gmail.com>
2024-06-27 09:49:53 +0900

    add: RoleとPlayerを定義

--- mylib/takema.c
+++ mylib/takema.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
 
+#define MAX_NAME_LENGTH 15
 char * hello_takema(){
     return "Hello. I'm takema!";
 }
+
+typedef enum {
+    VILLAGER,
+    WEREWOLF,
+    SEER, 
+    THIEF,
+} Role;
+
+typedef struct {
+    char name[MAX_NAME_LENGTH];
+    Role role;
+} Player;
