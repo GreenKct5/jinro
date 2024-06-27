29cac5fbf1605999e67f724eaa3fe5c3fc32e613
TakayamaH <823ab172@gmail.com>
2024-06-27 10:30:45 +0900

    add: RoleをStringに変換する関数を実装

--- mylib/takema.c
+++ mylib/takema.c
@@ -1,9 +1,5 @@
 #include <stdio.h>
-
 #define MAX_NAME_LENGTH 15
-char * hello_takema(){
-    return "Hello. I'm takema!";
-}
 
 typedef enum {
     VILLAGER,
@@ -16,3 +12,28 @@ typedef struct {
     char name[MAX_NAME_LENGTH];
     Role role;
 } Player;
+
+char * hello_takema(){
+    return "Hello. I'm takema!";
+}
+
+char * strRole(Role role){
+    switch (role)
+    {
+    case VILLAGER:
+        return "村人";
+        break;
+    case WEREWOLF:
+        return "人狼";
+        break;
+    case SEER:
+        return "占い師";
+        break;
+    case THIEF:
+        return "怪盗";
+        break;
+    default:
+        return "";
+        break;
+    }
+}
