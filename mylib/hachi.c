b9fe54d6a74223522353cab3a978056fd7bd3d66
Next to seven <huatianyingdou@gmail.com>
2024-07-04 13:27:19 +0900

    add:参加者の名前表示

--- mylib/hachi.c
+++ mylib/hachi.c
@@ -1,5 +1,81 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<fcntl.h>
+#include<unistd.h>
+#include<string.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<netdb.h>
+#include<arpa/inet.h>
+#include<time.h>
 
-char * hello_hachi(){
-    return "Hello. I'm hachi!";
+#define PORT    (in_port_t)50000
+#define BUF_LEN 512
+
+//#server
+void main(){
+    struct sockaddr_in me;
+    int soc_waiting;
+    int soc;
+    char buf[BUF_LEN];
+    fd_set readset;
+    int fd;
+
+    char myname[64] = "\0";
+    char villagersA[64] = "\0";
+    char villagersB[64] = "\0";
+    char villagersC[64] = "\0";
+    char chatroom[1000] = "\0";
+
+    write(1,"Input your name:", 16);
+    read(0, myname, 100);
+
+    FD_ZERO(&readset);
+    FD_SET(0,&readset);
+    FD_SET(soc,&readset);
+
+    myname[strlen(myname)-1] = '\0';
+
+    send(soc,myname,64,0);
+    recv(soc,villagersA,64,0);
+    recv(soc,villagersB,64,0);
+    recv(soc,villagersC,64,0);
+
+    snprintf(chatroom,512,"今回の村人は，%S，%S，%S，%Sです", myname, villagersA, villagersB, villagersC);
+    write(1,chatroom,512);
+    
+}
+
+//#client
+void main(){
+    struct sockaddr_in server;
+    int soc;
+    char ip_str[] = "server_address"
+    struct in_addr ip_addr;
+    char buf[BUF_LEN];
+    fd_set readset;
+    int fd;
+
+    char myname[64] = "\0";
+    char villagersA[64] = "\0";
+    char villagersB[64] = "\0";
+    char villagersC[64] = "\0";
+    char chatroom[1000] = "\0";
+
+    write(1,"Input your name:", 16);
+    read(0, myname, 100);
+
+    FD_ZERO(&readset);
+    FD_SET(0,&readset);
+    FD_SET(soc,&readset);
+
+    myname[strlen(myname)-1] = '\0';
+
+    send(soc,myname,64,0);
+    recv(soc,villagersA,64,0);
+    recv(soc,villagersB,64,0);
+    recv(soc,villagersC,64,0);
+
+    snprintf(chatroom,sizeof(chatroom),"今回の村人は，%S，%S，%S，%Sです", myname, villagersA, villagersB, villagersC);
+    write(1,chatroom,sizeof(chatroom));
 }
