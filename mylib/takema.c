c940aaba367b6419bacdbe041f2a27eeb81fe017
takema <823ab172@gmail.com>
2024-07-16 15:17:51 +0900

    add: 役職の振り分けをserverに記述

--- mylib/takema.c
+++ mylib/takema.c
@@ -1,18 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "takema.h"
 #define MAX_NAME_LENGTH 15
 
-typedef enum {
-    VILLAGER,
-    WEREWOLF,
-    SEER, 
-    THIEF,
-} Role;
-
-typedef struct {
-    char name[MAX_NAME_LENGTH];
-    Role role;
-    int sock;
-} Player;
 
 char * hello_takema(){
     return "Hello. I'm takema!";
@@ -59,4 +49,5 @@ int randomRole(Player *players,Role* trash,int playerNum){
         if (i < playerNum) players[i].role = roleCards[i];
     }
     for (int i = 0; i < 2;i++) trash[i] = roleCards[playerNum + i];
+    return 0;
 }
